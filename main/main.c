9ede8333c87ace3a5cdb85ccfb8cf56619fe6223
bignibba01 <davidemarchetti01@gmail.com>
2021-11-23 11:44:19 +0100

    programma completato -modificabile- ->legge direttamente una stringa dal file invece che carattere per carattere

--- main/main.c
+++ main/main.c
@@ -5,83 +5,100 @@ In questo esercizio, un testo è definito corrotto se contiene caratteri diversi 
 
 Attenzione: se un file non presenta caratteri corrotti e non ha il terminatore di riga non deve essere conteggiato come corrotto.
 Alla fine dell'esercizio visualizzare i nomi dei file corrotti.
+
+VENTRUCCI TOMAS, MARCHETTI DAVIDE, ZOLI FEREDEICO
 */
 
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS				//usata per visual studio in modo tale che esegua i metodi sprintf, strcat, fopen, strcpy; considerate non sicure
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
 char* changeFileName(char*, int);			//cambia il nome del file da aprire
 _Bool checkValidity(char);			//controlla se il carattere è valido o meno
+char* returnFileName(int);
 
 int main() {
-	FILE* fp;
-	char* name = "..\\File\\file";			//stringa da concatenare								//strionga finale
-	char result;
-	_Bool validity = false;			//flag per indicare se è presente il terminatore di riga (\n)
-	int numberFile = 1;		//001 è il primo file
 
-	char *path = changeFileName(name, numberFile);
-	printf("%s\n", path);
-	numberFile++;
+	int numberFile = 0;		//001 è il primo file
 
-	fp = fopen(path, "r");		//path è il nume del file che ogni volta viene modificato
+	while (numberFile <= 58) {
 
-	if (fp == NULL) {
-		printf("Errore -> Impossibile aprire il file.");
-		return 0;
+		FILE* fp;			//dichiaro il file da aprire
+		char* name = "..\\File\\file";			//stringa da concatenare
+		char result;		//carattere letto dal file
+		_Bool validity = false;			//flag per indicare se è presente il terminatore di riga (\n)
+
+		char* path = changeFileName(name, numberFile);		//path del file di testo 
+		char* fileName = returnFileName(numberFile);		//nome del file di testo incrementato
+		numberFile++;			//"indice" dei file di testo
+
+		fp = fopen(path, "r");		//path è il nume del file che ogni volta viene modificato
+
+		if (fp == NULL) {			//non ha avuto accesso al file
+			printf("Errore -> Impossibile aprire il file.");
+			return 0;
+		}
+
+		while ((result = fgetc(fp)) != EOF) {		//se leggendo i caratteri raggiunge la fine del file esce
+
+			if (result == '\n') {			//se è arrivato al terminatore di riga esce dal ciclo, se non è presente il terminatore continua fino alla fine del file
+				break;
+			}else if (!checkValidity(result)) {		//controlla la vilidità del carattere
+				validity = false;
+				break;
+			}
+			else
+				validity = true;
+
+
+		}
+
+		if (!validity) {			//se non è valido il file allora fa l'output
+			printf("%s -> non valido\n", fileName);
+		}
+		
+		validity = false;			//rinizializzo il flag a falso
+
+		fclose(fp);
 	}
-
-	while (!feof(fp)) {		//se raggiunge la fine del file esce
-		result = fgetc(fp);		//legge un singolo carattere
-
-		if (!checkValidity(result)) {		//controlla la vilidità del carattere
-			validity = false;
-			break;
-		} else
-			validity = true;
-		if (result == '\n')			//se è arrivato al terminatore di riga esce dal ciclo, se non è presente il terminatore continua fino alla fine del file
-			break;
-	}
-
-	if (validity) {
-		//stringa valida, contiene sia i caratteri giusti che il temrinatore
-		printf("valido");
-	}
-	else {
-		printf("non valido");
-	}
-
-	validity = false;
-
-
 
 	return 0;
 }
 
 char* changeFileName(char* name, int numberFile) {
-	char *path1;
-	char index[20];
+	char *path;
+	char index[8];			//nome del file di testo
 
-	sprintf(index, "%03d", numberFile);
+	sprintf(index, "%03d", numberFile);			//converto il numero intero in un carattere con almeno 3 cifre e se non ci sono ritora degli 0
 
 	strcat(index, ".txt");		//aggiungo il .txt al numero del file
-	printf("%s\n", index);
-
-	printf("%d\n", (strlen(name) + sizeof(index)));
-
-	path1 = (char*)malloc((strlen(name) + sizeof(index)));
-	strcpy(path1, name);
+	path = (char*)malloc((strlen(name) + sizeof(index)));				//alloco lo spazio per la stringa contenente il path del file
+	strcpy(path, name);
 	
-	strcat(path1, index);
-
-	return path1;
+	strcat(path, index);		//concateno il path iniziale con l' "indice" del file di testo
+	return path;
 }
 
 _Bool checkValidity(char c) {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == ' ' || c == '\'' || c == ',' || c == '\n')		//valore valido => 65-90 (A-Z), 97-122 (a-z), \n, "," , "\'"
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == ' ' || c == '\'' || c == ',')		//valore valido => 65-90 (A-Z), 97-122 (a-z), \n, "," , "\'"
 		return true;
 	else
 		return false;
+}
+
+char* returnFileName(int numberFile) {			//ritorno soltanto il nome del file di testo, con lo stesso principio di changeFileName
+	char* fileName;
+	char index[8];
+
+	sprintf(index, "%03d", numberFile);
+	strcat(index, ".txt");		//aggiungo il .txt al numero del file
+
+	fileName = (char*)malloc(strlen("file") + sizeof(index));			//alloco lo spazio per la stringa "file + numeroFile + .txt"
+
+	strcpy(fileName, "file");
+	strcat(fileName, index);
+
+	return fileName;
 }
